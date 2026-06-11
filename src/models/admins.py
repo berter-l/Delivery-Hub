bc723a72e448254d18c0b1b6513bced9fcdf7825
berter-l <asghijjahnbas@gmail.com>
2026-06-11 20:10:06 +0300

    .

--- src/models/admins.py
+++ src/models/admins.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.models.base import Base
 
 
-class admins(Base):
+class Admins(Base):
     __tablename__ = 'admins'
 
     id: Mapped[int] = mapped_column(primary_key=True)
