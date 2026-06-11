475b70c110567211bc8dbca7433045ae4da5bfa7
berter-l <asghijjahnbas@gmail.com>
2026-06-11 20:54:14 +0300

    add table orders

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,12 @@
 from src.models.admins import Admins
 from src.models.courier import Couriers
+from src.models.orders import Orders
 from src.models.partners import Partners
 
 
 __all__ = [
     'Couriers',
     'admins',
-    'Partners'
+    'Partners',
+    'Orders'
 ]
