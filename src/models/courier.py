80e3e80cbb9a2ec4e6e2e0ce084366c60083156f
berter-l <asghijjahnbas@gmail.com>
2026-06-15 19:56:59 +0300

    fixed the model

--- src/models/courier.py
+++ src/models/courier.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.types import pk_id
 from src.models.base import Base
@@ -11,12 +11,14 @@ class Couriers(Base):
     __tablename__ = "couriers"
 
     id: Mapped[pk_id]
-    name: Mapped[str] = mapped_column(String(100))
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
     phone: Mapped[str] = mapped_column(String(25))
     password_hash: Mapped[str] = mapped_column(String(255))
     rating: Mapped[float]
     is_active: Mapped[bool]
     fcm_token: Mapped[str] = mapped_column(String(255), unique=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     balance: Mapped[float]
     total_deliveries: Mapped[int]
+    orders: Mapped[list["Orders"]] = relationship(back_populates="courier")
