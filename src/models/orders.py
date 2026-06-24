622ea63a246bc912da448365ee124e026215256d
berter-l <asghijjahnbas@gmail.com>
2026-06-24 18:48:08 +0300

    changed orders

--- src/models/orders.py
+++ src/models/orders.py
@@ -1,21 +1,20 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.core.types import pk_id
+
 from src.models.base import Base
 
 
 class Orders(Base):
     __tablename__ = 'orders'
 
-    id: Mapped[pk_id]
     partner_id: Mapped[int] = mapped_column(ForeignKey('partners.id', ondelete='CASCADE'))
-    courier_id: Mapped[int] = mapped_column(ForeignKey('couriers.id'))
+    courier_id: Mapped[int | None] = mapped_column(ForeignKey('couriers.id'))
+    courier: Mapped["Couriers"] = relationship(back_populates="orders")
     pickup_address: Mapped[str]
     delivery_address: Mapped[str]
     delivery_fee: Mapped[float]
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
-    accepted_at: Mapped[datetime] = mapped_column(default=datetime.now())
-    delivered_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    accepted_at: Mapped[datetime | None]
+    delivered_at: Mapped[datetime | None]
