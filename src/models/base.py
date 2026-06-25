b4e1f16cf76edf766e2848e23a681d67f1129239
berter-l <asghijjahnbas@gmail.com>
2026-06-25 16:06:02 +0300

    add PermissionsMixin

--- src/models/base.py
+++ src/models/base.py
@@ -1,5 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from src.database import pk_id
+
+default_type = Annotated[bool, mapped_column(default=False)]
 
 
 class Base(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    id: Mapped[pk_id]
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+
+class PermissionsMixin():
+    is_admin: Mapped[default_type]
+    is_courier: Mapped[default_type]
+    is_partner: Mapped[default_type]
